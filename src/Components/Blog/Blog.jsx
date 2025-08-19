254045f9ea7152b6782490e62eecf97e45f2af10
nadim249 <mahmoudnadim47@gmail.com>
2025-08-19 21:59:34 +0600

    fix res issue

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -46,7 +46,7 @@ function Blog() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-3 sm:p-6">
+    <div className="max-w-4xl mx-auto px-3 sm:px-6 mt-14 sm:mt-16">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6">{blog.title}</h1>
       <div className="mb-4 sm:mb-6">
         <img 
@@ -67,7 +67,19 @@ function Blog() {
         </div>
       </div>
       <div 
-        className="prose max-w-none prose-sm sm:prose-base md:prose-lg prose-headings:text-xl sm:prose-headings:text-2xl md:prose-headings:text-3xl"
+        className="prose max-w-none prose-sm sm:prose-base md:prose-lg 
+        prose-headings:text-xl sm:prose-headings:text-2xl md:prose-headings:text-3xl
+        prose-p:text-sm sm:prose-p:text-base
+        prose-img:rounded-lg prose-img:my-2 prose-img:w-full prose-img:h-auto
+        prose-a:text-blue-600 hover:prose-a:text-blue-800
+        prose-ul:list-disc prose-ul:pl-5 prose-ol:pl-5
+        prose-li:mb-1 sm:prose-li:mb-2
+        [&_iframe]:w-full [&_iframe]:max-w-full [&_iframe]:aspect-video [&_iframe]:my-4
+        [&_video]:w-full [&_video]:max-w-full [&_video]:rounded-lg [&_video]:my-4
+        [&_pre]:overflow-x-auto [&_pre]:max-w-full [&_pre]:p-4 [&_pre]:text-sm
+        [&_blockquote]:text-sm [&_blockquote]:sm:text-base [&_blockquote]:border-l-4 [&_blockquote]:pl-4
+        [&_table]:overflow-x-auto [&_table]:w-full [&_table]:text-sm [&_table]:sm:text-base
+        [&_embed]:w-full [&_embed]:max-w-full [&_embed]:my-4"
         dangerouslySetInnerHTML={{ __html: blog.body_html }}
       />
     </div>
